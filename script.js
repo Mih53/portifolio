5a03f020abe5a465c609001f8a5c78a1a2843a82
Milena Osorio do Amaral <milenaamaral53@gmail.com>
2022-05-20 19:22:16 -0300

    :package:menu sidebar

---
+++ script.js
@@ -0,0 +1,7 @@
+/*ABRIR E FECHAR O MENU*/
+
+document.querySelector(".hamburguer").addEventListener("click", () =>
+
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+/*ABRIR E FECHAR O MENU*/
